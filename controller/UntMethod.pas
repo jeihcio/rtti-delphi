5e14fdb403cfd4854a5ce412fcc035e1c786f190
Jeihcio Francis <jeihcio.francis@hotmail.com>
2021-06-05 19:39:52 -0300

    Encapsulando o trecho de código que adiciona na treeview

--- controller/UntMethod.pas
+++ controller/UntMethod.pas
@@ -3,12 +3,13 @@ unit UntMethod;
 interface
 
 uses
-  UntTreeView, UntIDadosRtti;
+  UntTreeView, UntIDadosRtti, System.Rtti;
 
 type
   TMethod = class(TInterfacedObject, IDadosRtti)
   private
     FExibirResultado: TExibirResultadoTreeView;
+    procedure addNaTreeView(AExibirCamposHerdados: Boolean; Exemplo: TObject; Metodo: TRttiMethod; Tipo: TRttiType);
   public
     constructor Create(AExibirResultado: TExibirResultadoTreeView); reintroduce;
     procedure listar(AExibirCamposHerdados: Boolean);
@@ -17,7 +18,7 @@ type
 implementation
 
 uses
-  UntClasseExample, System.Rtti, UntRttiUtil, System.TypInfo, System.Classes;
+  UntClasseExample, UntRttiUtil, System.TypInfo, System.Classes;
 
 { TMethods }
 
@@ -26,52 +27,64 @@ begin
   FExibirResultado := AExibirResultado;
 end;
 
+procedure TMethod.addNaTreeView(AExibirCamposHerdados: Boolean;
+  Exemplo: TObject; Metodo: TRttiMethod; Tipo: TRttiType);
+var
+  Parametros: TArray<TRttiParameter>;
+  Parametro: TRttiParameter;
+  listaParametros: TStringList;
+begin
+  listaParametros := TStringList.Create;
+  Try
+    If isPularPropriedadeHerdada(AExibirCamposHerdados, Metodo, Tipo.Name) Then Exit;
+
+    Parametros := Metodo.GetParameters;
+    for Parametro in Parametros do
+      listaParametros.Add(Parametro.ToString);
+
+    if Length(Metodo.GetParameters) <= 0 then
+      Begin
+        FExibirResultado.addNaTreeView(Metodo.Visibility, Metodo.Name, [
+          'Tipo: ' + GetEnumName(TypeInfo(TMethodKind), Integer(Metodo.MethodKind)),
+          'Visibilidade: ' + GetEnumName(TypeInfo(TMemberVisibility), Integer(Metodo.Visibility)),
+          'Parametros: Nenhum'
+        ]);
+      End
+    Else
+      Begin
+        FExibirResultado.addNaMethodsTreeView(Metodo.Visibility, Metodo.Name, [
+          'Tipo: ' + GetEnumName(TypeInfo(TMethodKind), Integer(Metodo.MethodKind)),
+          'Visibilidade: ' + GetEnumName(TypeInfo(TMemberVisibility), Integer(Metodo.Visibility)),
+          'Parametros'
+        ], listaParametros.ToStringArray);
+
+        listaParametros.Clear;
+      End;
+  Finally
+    listaParametros.Free;
+  End;
+end;
+
 procedure TMethod.listar(AExibirCamposHerdados: Boolean);
 var
   Exemplo: TClasseExemplo;
   Contexto: TRttiContext;
   Tipo: TRttiType;
   Metodo: TRttiMethod;
-  Parametro: TRttiParameter;
-  Parametros: TArray<TRttiParameter>;
-  listaParametros: TStringList;
 begin
   Exemplo := TClasseExemplo.Create;
-  listaParametros := TStringList.Create;
   try
     Tipo := Contexto.GetType(Exemplo.ClassInfo);
 
     FExibirResultado.addNaTreeView(Tipo.Name);
     for Metodo in Tipo.GetMethods do
-      begin
-        If isPularPropriedadeHerdada(AExibirCamposHerdados, Metodo, Tipo.Name) Then Continue;
-
-        Parametros := Metodo.GetParameters;
-        for Parametro in Parametros do
-          listaParametros.Add(Parametro.ToString);
-
-        if Length(Metodo.GetParameters) <= 0 then
-          Begin
-            FExibirResultado.addNaTreeView(Metodo.Visibility, Metodo.Name, [
-              'Tipo: ' + GetEnumName(TypeInfo(TMethodKind), Integer(Metodo.MethodKind)),
-              'Visibilidade: ' + GetEnumName(TypeInfo(TMemberVisibility), Integer(Metodo.Visibility)),
-              'Parametros: Nenhum'
-            ]);
-          End
-        Else
-          Begin
-            FExibirResultado.addNaMethodsTreeView(Metodo.Visibility, Metodo.Name, [
-              'Tipo: ' + GetEnumName(TypeInfo(TMethodKind), Integer(Metodo.MethodKind)),
-              'Visibilidade: ' + GetEnumName(TypeInfo(TMemberVisibility), Integer(Metodo.Visibility)),
-              'Parametros'
-            ], listaParametros.ToStringArray);
-
-            listaParametros.Clear;
-          End;
-      end;
+      addNaTreeView(
+        AExibirCamposHerdados,
+        Exemplo,
+        Metodo,
+        Tipo);
   finally
     Exemplo.Free;
-    listaParametros.Free;
   end;
 end;
 
